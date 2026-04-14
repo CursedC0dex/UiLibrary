1c6322cf59ad1f0f115a8517a1163ece9b2126e8
CursedC0dex <vejchodsky.petr@outlook.cz>
2026-04-14 22:21:26 +0200

    Update source.lua

--- source.lua
+++ source.lua
@@ -42,12 +42,11 @@ function cursed:CreateLib(options)
 		-- Main Frame
 	do
 		-- StarterGui.MyLibrary
-		
+		GUI["1"] = Instance.new("ScreenGui", game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"));
 		GUI["1"]["IgnoreGuiInset"] = true;
 		GUI["1"]["Enabled"] = true;
 		GUI["1"]["ScreenInsets"] = Enum.ScreenInsets.DeviceSafeInsets;
 		GUI["1"]["Name"] = [[MyLibrary]];
-		GUI["1"] = Instance.new("ScreenGui", game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"));
 		GUI["1"]["ResetOnSpawn"] = false
 		GUI["1"]["DisplayOrder"] = 999;
 
